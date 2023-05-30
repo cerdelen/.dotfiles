c682886a592d5be6c24598b601ede2e78d157f0c
Ubuntu <ubuntu@ip-172-31-42-248.eu-north-1.compute.internal>
2023-05-30 21:05:45 +0000

    tmuxifier

--- tmux/init.sh
+++ tmux/init.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-cp ./.tmux.conf ~
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source ~/.tmux.conf
+if [ ! -d ~/.tmuxifier ]
+then 
+    git clone https://github.com/jimeh/tmuxifier ~/.tmuxifier
+
+    echo "PATH=\$PATH:\$HOME/.tmuxifier/bin" >> ~/.bashrc
+    echo "eval \$(tmuxifier init -)" >> ~/.bashrc
+    echo "export EDITOR=nvim" >> ~/.bashrc
+else
+    echo "tmuxifier already exists"
+fi
+
+if [ ! -f ~/.tmux.conf ]
+then
+    cp ./.tmux.conf ~
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux source ~/.tmux.conf
+else
+    echo "tmux already setup"
+fi
