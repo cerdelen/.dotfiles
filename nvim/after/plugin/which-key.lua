e6e2d37e3a8494b7662402f34a8e88e5c2c456d0
cerdelen <cedric.erdelen@gmail.com>
2023-09-11 10:46:33 +0200

    which key mappings

--- nvim/after/plugin/which-key.lua
+++ nvim/after/plugin/which-key.lua
@@ -1,11 +1,13 @@
 local wk = require("which-key")
 
 wk.register({
-  p = {
-    name = "Finding Files", -- optional group name
-    f = "Telescope", -- just a label. don't create any mapping
-    s = "Grep all Files", -- same as above
-    v = "Vim Filetree",
-    g = "Telescope Git",
-  },
+    a = "add current buffer to Harpoon (only in buffer)",
+    u = "UndooTree Toggle",
+    p = {
+        name = "Finding Files", -- optional group name
+        f = "Telescope", -- just a label. don't create any mapping
+        s = "Grep all Files", -- same as above
+        v = "Vim Filetree",
+        g = "Telescope Git",
+    },
 }, { prefix = "<leader>" })
