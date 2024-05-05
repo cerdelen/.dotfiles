5b1c8bb84369e578ac2dd24904c433edbccb7bdd
cerdelen <cedric.erdelen@gmail.com>
2024-05-05 19:01:12 +0200

    basic lsp setup

--- new_nvim/lua/cerdelen/init.lua
+++ new_nvim/lua/cerdelen/init.lua
@@ -1,3 +1,14 @@
 require("cerdelen.options")
 require("cerdelen.keymaps")
 require("cerdelen.lazy_init")
+
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- removing any whitespaces at end of line
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
