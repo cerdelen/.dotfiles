9983fbe9f2100d4c06db87b981d19e963b7114ff
cerdelen <cedric.erdelen@gmail.com>
2024-07-06 19:25:14 +0200

    illuminate bare bones

---
+++ nvim/.config/nvim/lua/cerdelen/plugins/illuminate.lua
@@ -0,0 +1,14 @@
+return {
+    -- "RRethy/vim-illuminate",
+    --
+    -- config = function()
+    --     require('illuminate').configure({
+    --         providers = {
+    --             'lsp',
+    --             'treesitter',
+    --             'regex',
+    --         },
+    --         min_count_to_highlight = 2,
+    --     })
+    -- end
+}
