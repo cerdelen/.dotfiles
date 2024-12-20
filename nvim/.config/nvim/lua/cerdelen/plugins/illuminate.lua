178c2b2fe50587590a0ade0be030acb7eb75667e
cerdelen <cedric.erdelen@gmail.com>
2024-12-20 12:38:17 +0100

    dap && illuminate

--- nvim/.config/nvim/lua/cerdelen/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/cerdelen/plugins/illuminate.lua
@@ -1,14 +1,19 @@
 return {
-    -- "RRethy/vim-illuminate",
-    --
-    -- config = function()
-    --     require('illuminate').configure({
-    --         providers = {
-    --             'lsp',
-    --             'treesitter',
-    --             'regex',
-    --         },
-    --         min_count_to_highlight = 2,
-    --     })
-    -- end
+    "RRethy/vim-illuminate",
+
+    config = function()
+        require('illuminate').configure({
+            providers = {
+                'lsp',
+                'treesitter',
+                'regex',
+            },
+            min_count_to_highlight = 2,
+            vim.cmd [[
+              highlight IlluminatedWordText guibg=#3c3836 gui=none
+              highlight IlluminatedWordRead guibg=#3c3836 gui=none
+              highlight IlluminatedWordWrite guibg=#504945 gui=none
+            ]]
+        })
+    end
 }
